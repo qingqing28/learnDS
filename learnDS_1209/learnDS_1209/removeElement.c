403c13f01150e0586ed405b03497adda93a50a8b
PQQ <qqpengyc@163.com>
2020-12-19 23:51:13 +0800

    删除链表中值为val的所有节点

---
+++ learnDS_1209/learnDS_1209/removeElement.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+ 
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode* cur, * prev;
+    cur = head;
+    prev = NULL;
+    while (cur) {
+        //数据等于val，则删除该节点
+        if (cur->val == val) {
+            struct ListNode* next = cur->next;
+            if (prev == NULL) {
+                head = next;
+            }
+            else {
+                prev->next = next;
+            }
+            free(cur);
+            cur = next;
+        }
+        else {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
+}
