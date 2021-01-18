417dfebb5b8c920c4728bf99281b47295ce529e7
PQQ <qqpengyc@163.com>
2021-01-18 23:43:45 +0800

    队列的数组实现

---
+++ learnDS_201220/learnDS_201220/queuearr.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int QDataType;
+
+typedef struct Queue {
+	QDataType* data;//存储队列的数据
+	int rear;  //表示队尾，指向队列最后一个元素的下一个位置
+	int capacity;//表示数组的大小
+}Queue;
+
+// 初始化队列
+void QueueInit(Queue* q) {
+	q->data = NULL;
+	q->rear = 0;
+	q->capacity = 0;
+}
+//检查容量
+void checkCapacity(Queue* q) {
+	if (q == NULL)
+		return;
+	if (q->rear == q->capacity) {
+		//进行扩容
+		int newCapacity = (q->capacity == 0) ? 1 : 2 * q->capacity;
+		q->data = (QDataType*)realloc(q->data , sizeof(QDataType) * newCapacity);
+		q->capacity = newCapacity;
+	}
+}
+
+// 队尾入队列
+void QueuePush(Queue* q, QDataType data) {
+	if (q == NULL)
+		return;
+	checkCapacity(q);
+	q->data[q->rear] = data;
+	q->rear++;
+}
+
+// 检测队列是否为空，如果为空返回非零结果，如果非空返回0
+int QueueEmpty(Queue* q) {
+	if (q->rear == 0)
+		return 1;
+	return 0;
+}
+
+// 队头出队列(头插）
+void QueuePop(Queue* q) {
+	if (q == NULL || QueueEmpty(q) == 1)
+		return;
+	//将数据向后移动
+	for (int i = 1; i < q->rear; i++) {
+		q->data[i - 1] = q->data[i];
+	}
+	q->rear--;
+}
+
+// 获取队列头部元素
+QDataType QueueFront(Queue* q) {
+	return q->data[0];
+}
+// 获取队列队尾元素
+QDataType QueueBack(Queue* q) {
+	return q->data[q->rear - 1];
+}
+// 获取队列中有效元素个数
+int QueueSize(Queue* q) {
+	return q->rear;
+}
+
+// 销毁队列
+void QueueDestroy(Queue* q) {
+	free(q->data);
+	q->data = NULL;
+	q->capacity = 0;
+	q->rear = 0;
+}
+
+int main() {
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	//队列为1 2 3
+	printf("队头=%d\n", QueueFront(&q));//队头为1
+	printf("队尾=%d\n", QueueBack(&q));//队尾为3
+	printf("队列长度=%d\n", QueueSize(&q));//队列长度为3
+	printf("队列是否为空：%d\n", QueueEmpty(&q));//队列不为空
+	//出队
+	QueuePop(&q);
+	printf("队头=%d\n", QueueFront(&q));//队头为2
+	printf("队尾=%d\n", QueueBack(&q));//队尾为3
+	QueuePop(&q);
+	printf("队头=%d\n", QueueFront(&q));//队头为3
+	printf("队尾=%d\n", QueueBack(&q));//队尾为3
+	QueuePop(&q);
+	printf("队列是否为空：%d\n", QueueEmpty(&q));//队列不为空
+	return 0;
+}
