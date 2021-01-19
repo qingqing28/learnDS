b005a35da1ceec06fbe6ba4135d86c8e479a5415
PQQ <qqpengyc@163.com>
2021-01-19 22:32:32 +0800

    数组实现栈

---
+++ learnDS_201220/learnDS_201220/stack.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"stack.h"
+
+void stackInit(Stack* st) {
+	if (st == NULL)
+		return;
+	st->data = NULL;
+	//栈中没有存储任何元素，栈顶指向-1
+	st->top = -1;
+	st->capacity = 0;
+}
+
+void CheckCapacity(Stack* st) {
+	if (st == NULL)
+		return;
+	//若数组存储满了，则进行扩容
+	if (st->top == (st->capacity) - 1) {
+		int newCapacity = st->capacity == 0 ? 1 : st->capacity * 2;
+		//将st->data数组扩容到sizeof(STDataType)*newCapacity个字节大小
+		st->data = (STDataType*)realloc(st->data, sizeof(STDataType) * newCapacity);
+		st->capacity = newCapacity;
+	}
+}
+
+void stackPush(Stack* st, STDataType val) {
+	if (st == NULL)
+		return;
+	//检查栈是否满了
+	CheckCapacity(st);
+	//进行入栈，栈顶+1 ，存入数据
+	st->top++;
+	st->data[st->top] = val;
+}
+
+void stackPop(Stack* st) {
+	//若栈中无数据则进行返回
+	if (st == NULL || st->top == -1) {
+		return;
+	}
+	//栈顶-1
+	st->top--;
+}
+
+//获取栈顶元素
+STDataType stackTop(Stack* st) {
+	//前提是栈中有有效元素
+	return st->data[st->top];
+}
+
+//获取栈中的有效元素个数
+int stackSize(Stack* st) {
+	if (st == NULL)
+		return 0;
+	return (st->top) + 1;
+}
+
+//检测栈是否为空，如果为空返回非零结果，如果不为空返回0
+int stackEmpty(Stack* st) {
+	if (st == NULL || st->top == -1)
+		return 1;
+	return 0;
+}
+
+//销毁栈
+void stackDestroy(Stack* st) {
+	free(st->data);
+	st->data = NULL;
+	st->top = -1;
+	st->capacity = 0;
+}
